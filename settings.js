dd2e8dcc010efeaeafe3737d5b00f6122073653e
essaouidi-bls <tourismebls2@gmail.com>
2026-07-26 12:36:17 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Essaouidi ai',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Essaouidi ai",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '212648753294', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.7",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/mruniquehacker/Essaouidi ai-MD/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
